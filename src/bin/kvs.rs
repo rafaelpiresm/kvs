fdd8b96b84de8243d068600719232832a2e975a8
Rafael Mendes <rafaelpiresm@gmail.com>
2021-04-25 23:12:49 -0300

    Creating an App with clap crate, and this is really awesome =]

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,52 @@
+use clap::{Arg, App, SubCommand};
+use std::process::exit;
+
 fn main() {
-    println!("Hello, World!");
+    const VERSION: &str = "0.1";
+
+    let matches = App::new("kvs inMemory utility") 
+                    .version(VERSION)
+                    .author("Rafael Mendes, based on PingCAP talent-plan")
+                    .about("Stores and queries values on memory")
+                    .subcommand(SubCommand::with_name("V")
+                        .about("Print version of kvs")
+                        .version(VERSION))
+                    .subcommand(SubCommand::with_name("get")
+                        .about("Read data from memory based on key")
+                        .version(VERSION)
+                        .arg(Arg::with_name("key")
+                            .help("Key to lookup on memstore")
+                            .required(true)))
+                    .subcommand(SubCommand::with_name("set")
+                        .about("Write data into memory store")
+                        .version(VERSION)
+                        .arg(Arg::with_name("key")
+                            .help("Key to persist into memstore")
+                            .required(true))
+                        .arg(Arg::with_name("value")
+                            .help("Value to persist data into memstore")
+                            .required(true)))
+                    .subcommand(SubCommand::with_name("rm")
+                        .about("Remove data from memstore")
+                        .version(VERSION)
+                        .arg(Arg::with_name("key")
+                            .help("Key to remove data from memstore")
+                            .required(true))).get_matches();
+
+    match matches.subcommand() {
+        ("get", Some(_matches)) => {
+            eprintln!("unimplemented!");
+            exit(1);
+        }
+        ("set", Some(_matches)) => {
+            eprintln!("unimplemented!");
+            exit(1);
+        }        
+        ("rm", Some(_matches)) => {
+            eprintln!("unimplemented!");
+            exit(1);
+        }
+        _ => unreachable!(),
+    }
+        
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -117,8 +117,8 @@ fn cli_invalid_subcommand() {
 fn get_stored_value() {
     let mut store = KvStore::new();
 
-    store.set("key1".to_owned(), "value1".to_owned());
-    store.set("key2".to_owned(), "value2".to_owned());
+    store.set("key1".to_string(), "value1".to_string());
+    store.set("key2".to_string(), "value2".to_string());
 
     assert_eq!(store.get("key1".to_owned()), Some("value1".to_owned()));
     assert_eq!(store.get("key2".to_owned()), Some("value2".to_owned()));
