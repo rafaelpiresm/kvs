108fb32ccc982ccdfee446696fc403b214fc6452
Rafael Mendes <rafaelpiresm@gmail.com>
2021-04-29 21:45:59 -0300

    Tests is building now

--- src/kv.rs
+++ src/kv.rs
@@ -1,10 +1,15 @@
 use std::collections::HashMap;
+use std::path::Path;
+use crate::{KvsError, Result};
 
 ///KvStore eh a estrutura de dados básica
 #[derive(Default)]
 pub struct KvStore {
     map: HashMap<String, String>,
 }
+
+///Soh pra buildar o teste
+//pub enum Result<T> {}
 
 impl KvStore {
     ///Creates KvStore
@@ -15,17 +20,24 @@ impl KvStore {
     }
 
     ///Busca conteúdo dentro da KvStore    
-    pub fn get(&mut self, key: String) -> Option<String> {
-        self.map.get(&key).cloned()
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        Ok(self.map.get(&key).cloned())
     }
 
     ///Armazena conteúdo dentro da KvStore
-    pub fn set(&mut self, key: String, value: String) {
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
         self.map.insert(key, value);
+        Ok(())
     }
 
     ///Remove conteúdo de dentro da KvStore
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.map.remove(&key);
+        Ok(())
+    }
+
+    ///Open a file 
+    pub fn open(_path: &Path) -> Result<KvStore> {
+        unimplemented!()
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,6 @@
 //! A simple key-value store
 
 pub use kv::KvStore;
+pub use error::{KvsError, Result};
 mod kv;
+mod error;
